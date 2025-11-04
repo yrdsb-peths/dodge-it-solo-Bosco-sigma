4d058983d2b6ec9a2f8c5676f2c424a576852eb7
Bosco-sigma <fbmeng2009@gmail.com>
2025-11-04 13:53:00 -0500

    AddedHero2

---
+++ Hero2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hero2 here.
+ * 
+ * @author Bosco Meng
+ * @version 11/4/2025
+ */
+public class Hero2 extends Actor
+{
+    /**
+     * Act - do whatever the Hero2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(-1);
+    }
+}
