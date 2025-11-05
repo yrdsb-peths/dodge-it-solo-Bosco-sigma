6112fd0d47abd71e464d19c15dbe1b4afe655b6d
Bosco-sigma <fbmeng2009@gmail.com>
2025-11-05 14:00:06 -0500

    addObstacles

--- MyWorld.java
+++ MyWorld.java
@@ -2,7 +2,7 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 400, 1);
+        super(800, 600, 1);
         
         Hero2 hero = new Hero2();
         addObject(hero, 200, 200);
@@ -16,6 +16,10 @@ public class MyWorld extends World {
         Apple apple3 = new Apple();
         addObject(apple3, 300, 100);
         
+        MoreObstacles rock = new MoreObstacles();
+        addObject(rock, 400, 200);
         
+        MoreObstacles rock2 = new MoreObstacles();
+        addObject(rock2, 500, 300);
     }
 }
